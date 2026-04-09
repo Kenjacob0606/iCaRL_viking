b752c36c520d7888a05f01a4705166259d287255
kenjacob <kenjacob0606@gmail.com>
2026-04-09 18:43:54 +0100

    .

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epo
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(5): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
