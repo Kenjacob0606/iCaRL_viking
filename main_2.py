b27d0c3113b6edd125896ae8b98045bdbbaf7600
kenjacob <kenjacob0606@gmail.com>
2026-04-14 20:10:26 +0100

    .

--- main_2.py
+++ main_2.py
@@ -6,54 +6,54 @@ from ResNet import resnet50_cbam
 import torch
 import time
 
-#	CIFAR100_mem=7.5K_def
+#cifar100_class=5_def
 
-numclass=10#num of classes learned initially, will be updated in incremental learning
-feature_extractor=resnet18_cbam() #try other resnets
-img_size=32
-batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 7500
-epochs=70 #was 100
-learning_rate=2.0
-file=1
-dataset='CIFAR100' #try other dataset
-train_no = 1
+# numclass=5#num of classes learned initially, will be updated in incremental learning
+# feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
+# img_size=32
+# batch_size=128  
+# task_size=5 #num of classes learned each task
+# memory_size= 2000
+# epochs=70 #was 100
+# learning_rate=2.0
+# file=1
+# dataset='CIFAR100' #try other dataset
+# train_no = 1
 
-model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no) #try other dataset
-#model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
+# model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no) #try other dataset
+# #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
-start_time = time.time()
-for i in range(10): #was 10,5
-    # if i==0:
-    #     start_time = time.time()
-    task_start_time = time.time()
-    model.beforeTrain()
-    accuracy=model.train()
-    model.afterTrain(accuracy)
-    task_end_time = time.time()
-    filename = f'CIFAR100_mem=7.5K_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
-    torch.save((task_end_time - task_start_time), filename)
-    # if i==9:
-end_time = time.time()
+# start_time = time.time()
+# for i in range(20): #was 10,5
+#     # if i==0:
+#     #     start_time = time.time()
+#     task_start_time = time.time()
+#     model.beforeTrain()
+#     accuracy=model.train()
+#     model.afterTrain(accuracy)
+#     task_end_time = time.time()
+#     filename = f'cifar100_class=5_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
+#     torch.save((task_end_time - task_start_time), filename)
+#     # if i==9:
+# end_time = time.time()
 
-# print('Total training time: {:.2f} seconds'.format(end_time - start_time))
-filename2 = f'CIFAR100_mem=7.5K_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
-torch.save((end_time - start_time), filename2)
+# # print('Total training time: {:.2f} seconds'.format(end_time - start_time))
+# filename2 = f'cifar100_class=5_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
+# torch.save((end_time - start_time), filename2)
 
-del model
-torch.cuda.empty_cache()
+# del model
+# torch.cuda.empty_cache()
 
 #####################################################################################################################################
 
 #TRAIN2
 
-numclass=10#num of classes learned initially, will be updated in incremental learning
-feature_extractor=resnet18_cbam() #try other resnets
+numclass=5#num of classes learned initially, will be updated in incremental learning
+feature_extractor=resnet18_cbam(num_classes = numclass) #try other resnets
 img_size=32
 batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 7500
+task_size=5 #num of classes learned each task
+memory_size= 2000
 epochs=70 #was 100
 learning_rate=2.0
 file=1
@@ -64,7 +64,7 @@ model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epo
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(20): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
@@ -72,13 +72,13 @@ for i in range(10): #was 10,5
     accuracy=model.train()
     model.afterTrain(accuracy)
     task_end_time = time.time()
-    filename = f'CIFAR100_mem=7.5K_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
+    filename = f'cifar100_class=5_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
     torch.save((task_end_time - task_start_time), filename)
     # if i==9:
 end_time = time.time()
 
 # print('Total training time: {:.2f} seconds'.format(end_time - start_time))
-filename2 = f'CIFAR100_mem=7.5K_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
+filename2 = f'cifar100_class=5_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
 torch.save((end_time - start_time), filename2)
 
 del model
@@ -88,12 +88,12 @@ torch.cuda.empty_cache()
 
 #TRAIN3
 
-numclass=10#num of classes learned initially, will be updated in incremental learning
-feature_extractor=resnet18_cbam() #try other resnets
+numclass=5#num of classes learned initially, will be updated in incremental learning
+feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
 img_size=32
 batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 7500
+task_size=5 #num of classes learned each task
+memory_size= 2000
 epochs=70 #was 100
 learning_rate=2.0
 file=1
@@ -104,7 +104,7 @@ model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epo
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(20): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
@@ -112,13 +112,13 @@ for i in range(10): #was 10,5
     accuracy=model.train()
     model.afterTrain(accuracy)
     task_end_time = time.time()
-    filename = f'CIFAR100_mem=7.5K_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
+    filename = f'cifar100_class=5_def/model/task_{i}_training_time={task_end_time - task_start_time:.2f}_train={train_no}.txt'     #modify
     torch.save((task_end_time - task_start_time), filename)
     # if i==9:
 end_time = time.time()
 
 # print('Total training time: {:.2f} seconds'.format(end_time - start_time))
-filename2 = f'CIFAR100_mem=7.5K_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
+filename2 = f'cifar100_class=5_def/model/total_training_time= {end_time - start_time:.2f}_train={train_no}.txt'        #modify
 torch.save((end_time - start_time), filename2)
 
 
