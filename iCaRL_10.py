5e5af8ebdcdbf2b101cbf1a18d71d9b6ef39678b
kenjacob <kenjacob0606@gmail.com>
2026-04-15 12:06:37 +0100

    .

--- iCaRL_10.py
+++ iCaRL_10.py
@@ -65,9 +65,9 @@ class iCaRLmodel:
                                             transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))]) #CIFAR100
             
             self.train_transform = transforms.Compose([#transforms.Resize(img_size),
-                                                    transforms.RandomCrop((32,32),padding=4),
+                                                    # transforms.RandomCrop((32,32),padding=4),
                                                     transforms.RandomHorizontalFlip(p=0.5),
-                                                    transforms.ColorJitter(brightness=0.24705882352941178),
+                                                    # transforms.ColorJitter(brightness=0.24705882352941178),
                                                     transforms.ToTensor(),
                                                     transforms.Normalize((0.5071, 0.4867, 0.4408), (0.2675, 0.2565, 0.2761))]) #CIFAR100
             
