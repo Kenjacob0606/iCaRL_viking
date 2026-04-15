c155c07dbc4528e5b220662233f1116bb29619d5
kenjacob <kenjacob0606@gmail.com>
2026-04-16 00:23:36 +0100

    .

--- main.py
+++ main.py
@@ -6,28 +6,28 @@ from ResNet import resnet50_cbam
 import torch
 import time
 
-#"cifar100_epoch=50_def"
+#"cifar10_class=2_def"
 
 #TRAIN 1
 
-numclass=10#num of classes learned initially, will be updated in incremental learning   
+numclass=2#num of classes learned initially, will be updated in incremental learning   
 feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
 img_size=32
 batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 2000
-epochs=50 #was 100
+task_size=2 #num of classes learned each task
+memory_size= 1000
+epochs=70 #was 100
 learning_rate=2.0
 file=1
-dataset='CIFAR100' #try other dataset
+dataset='CIFAR10' #try other dataset
 train_no = 1
-filenames = "cifar100_epoch=50_def"
+filenames = "cifar10_class=2_def"
 
 model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(5): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
@@ -50,24 +50,24 @@ torch.cuda.empty_cache()
 ######################################################################################################################################################
 #TRAIN 2
 
-numclass=10#num of classes learned initially, will be updated in incremental learning
+numclass=2#num of classes learned initially, will be updated in incremental learning
 feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
 img_size=32
 batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 2000
-epochs=50 #was 100
+task_size=2 #num of classes learned each task
+memory_size= 1000
+epochs=70 #was 100
 learning_rate=2.0
 file=1
-dataset='CIFAR100' #try other dataset
+dataset='CIFAR10' #try other dataset
 train_no = 2
-filenames = "cifar100_epoch=50_def"
+filenames = "cifar10_class=2_def"
 
 model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
 #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(5): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
@@ -89,23 +89,23 @@ torch.cuda.empty_cache()
 ######################################################################################################################################################
 #TRAIN 3
 
-numclass=10#num of classes learned initially, will be updated in incremental learning
+numclass=2#num of classes learned initially, will be updated in incremental learning
 feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
 img_size=32
 batch_size=128  
-task_size=10 #num of classes learned each task
-memory_size= 2000
-epochs=50 #was 100
+task_size=2 #num of classes learned each task
+memory_size= 1000
+epochs=70 #was 100
 learning_rate=2.0
 file=1
-dataset='CIFAR100' #try other dataset
+dataset='CIFAR10' #try other dataset
 train_no = 3
-filenames = "cifar100_epoch=50_def"
+filenames = "cifar10_class=2_def"
 
 model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
 
 start_time = time.time()
-for i in range(10): #was 10,5
+for i in range(5): #was 10,5
     # if i==0:
     #     start_time = time.time()
     task_start_time = time.time()
