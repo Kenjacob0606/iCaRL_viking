47a8aa65abebf208965159b440177d89f8578e93
kenjacob <kenjacob0606@gmail.com>
2026-04-16 23:21:37 +0100

    .

--- iCaRL_6.py
+++ iCaRL_6.py
@@ -309,7 +309,7 @@ class iCaRLmodel:
         self.old_model.eval()
         
 #GRAPHS
-        if self.task_num==5:                    #Changable param depending on how many class each task
+        if self.task_num==10:                    #Changable param depending on how many class each task
             if self.task_num==10 and self.dataset=='MNIST':     
                 plt.plot(self.task_list, self.accuracy_list, "g+")
                 plt.xticks(range(len(self.accuracy_list)+1))
@@ -317,8 +317,9 @@ class iCaRLmodel:
                 plt.xlabel("Task")
                 plt.ylabel("Accuracy")
                 plt.title("Accuracy vs Tasks")
-                filename3 = f'{self.filenames}/model/accuracy_vs_tasks.png'
-                plt.savefig(filename3)                 
+                filename3 = f'{self.filenames}/model/accuracy_vs_tasks-{self.train_no}.png'
+                plt.savefig(filename3)   
+                plt.clf()              
                 # plt.show()
                 plt.plot(self.task_list, self.accuracy_list, "g+-")
                 plt.xticks(range(len(self.accuracy_list)+1))
@@ -326,8 +327,9 @@ class iCaRLmodel:
                 plt.xlabel("Task")
                 plt.ylabel("Accuracy")
                 plt.title("Accuracy vs Tasks")
-                filename4 = f'{self.filenames}/model/accuracy_vs_tasks.png'
-                plt.savefig(filename4)                 
+                filename4 = f'{self.filenames}/model/accuracy_vs_tasks-{self.train_no}.png'
+                plt.savefig(filename4)  
+                plt.clf()               
             else:
                 plt.plot(self.task_list, self.accuracy_list, "g+")
                 plt.xticks(range(len(self.accuracy_list)+1))
@@ -335,8 +337,9 @@ class iCaRLmodel:
                 plt.xlabel("Task")
                 plt.ylabel("Accuracy")
                 plt.title("Accuracy vs Tasks")
-                filename3 = f'{self.filenames}/model/accuracy_vs_tasks.png'
-                plt.savefig(filename3)                 
+                filename3 = f'{self.filenames}/model/accuracy_vs_tasks-{self.train_no}.png'
+                plt.savefig(filename3)   
+                plt.clf()              
                 # plt.show()
                 plt.plot(self.task_list, self.accuracy_list, "g+-")
                 plt.xticks(range(len(self.accuracy_list)+1))
@@ -344,7 +347,7 @@ class iCaRLmodel:
                 plt.xlabel("Task")
                 plt.ylabel("Accuracy")
                 plt.title("Accuracy vs Tasks")
-                filename4 = f'{self.filenames}/model/accuracy_vs_tasks.png'
+                filename4 = f'{self.filenames}/model/accuracy_vs_tasks-{self.train_no}.png'
                 plt.savefig(filename4)                 
                 # plt.show()
                 # print(len(self.class_mean_set))
