44df6be26b637212a11906ceb6a83028d144b8f8
kenjacob <kenjacob0606@gmail.com>
2026-04-18 21:54:08 +0100

    .

--- main_randomHeard.py
+++ main_randomHeard.py
@@ -6,7 +6,7 @@ from ResNet import resnet50_cbam
 import torch
 import time
 
-#"cifar100_random_herd"
+#	cifar100_mem=500_random_herd
 
 for train_no in range(1,4):
     dataset='CIFAR100' #try other dataset
@@ -19,11 +19,11 @@ for train_no in range(1,4):
     img_size=32
     batch_size=128  
     task_size=10      #num of classes learned each task
-    memory_size= 2000
+    memory_size= 500
     epochs=70 #was 100
     learning_rate=2.0
     file=1
-    filenames = "cifar100_random_herd"
+    filenames = "cifar100_mem=500_random_herd"
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
     #model.model.load_state_dict(torch.load('model/ownTry_accuracy:84.000_KNN_accuracy:84.000_increment:10_net.pkl'))
