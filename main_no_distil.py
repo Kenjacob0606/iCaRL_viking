c7f7feeeeebf6d20dfa0166073263182a31366a8
kenjacob <kenjacob0606@gmail.com>
2026-04-18 01:42:55 +0100

    .

--- main_no_distil.py
+++ main_no_distil.py
@@ -9,8 +9,13 @@ import time
 #   "CIFAR100_mem=2K_def_no_distil"
 
 for train_no in range (1,4):
+    dataset='CIFAR100' #try other dataset
     numclass=10      #num of classes learned initially, will be updated in incremental learning
-    feature_extractor=resnet18_cbam(num_classes=numclass) #try other resnets
+    if dataset == 'CIFAR100':
+        numclasses = 100
+    else:
+        numclasses = 10
+    feature_extractor=resnet18_cbam(num_classes=numclasses) #try other resnets
     img_size=32
     batch_size=128  
     task_size=10         #num of classes learned each task
@@ -18,7 +23,6 @@ for train_no in range (1,4):
     epochs=70 #was 100
     learning_rate=2.0
     file=1
-    dataset='CIFAR100' #try other dataset
     filenames = "CIFAR100_mem=2K_def_no_distil"
 
 
