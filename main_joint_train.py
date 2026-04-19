0152a486d61702d3bd0c557e2cfdf70fdf791051
kenjacob <kenjacob0606@gmail.com>
2026-04-19 23:20:38 +0100

    .

--- main_joint_train.py
+++ main_joint_train.py
@@ -8,7 +8,7 @@ import time
 
 #   "MNIST_class=1_mem=20K_def"
 
-for train_no in range (2,4):
+for train_no in range (1,4):
     dataset='MNIST' #try other dataset
     numclass=1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
@@ -20,10 +20,10 @@ for train_no in range (2,4):
     batch_size=128  
     task_size=1         #num of classes learned each task
     memory_size= 50000
-    epochs=70 #was 100
+    epochs=15 #was 100
     learning_rate=2.0
     file=1
-    filenames = "MNIST_class=1_joint_def"
+    filenames = "MNIST_class=1_epo15_joint_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
