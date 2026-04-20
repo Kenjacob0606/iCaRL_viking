43be6aa2f48b9505620c74bfc90e2d0e4478be85
kenjacob <kenjacob0606@gmail.com>
2026-04-20 21:38:33 +0100

    .

--- main_5.py
+++ main_5.py
@@ -1,4 +1,4 @@
-from iCaRL_5 import iCaRLmodel
+from iCaRL_cifar10_epo50 import iCaRLmodel
 from ResNet import resnet18_cbam
 from ResNet import resnet18_MNIST_cbam
 from ResNet import resnet34_cbam
@@ -9,8 +9,8 @@ import time
 #cifar100_mem=100
 
 for train_no in range(1,4):
-    dataset='CIFAR100' #try other dataset
-    numclass=10      #num of classes learned initially, will be updated in incremental learning
+    dataset='CIFAR10' #try other dataset
+    numclass=1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
         numclasses = 100
     else:
@@ -18,12 +18,12 @@ for train_no in range(1,4):
     feature_extractor=resnet18_cbam(num_classes=numclasses) #try other resnets
     img_size=32
     batch_size=128  
-    task_size=10 #num of classes learned each task
-    memory_size= 100
-    epochs=70 #was 100
+    task_size=1 #num of classes learned each task
+    memory_size= 50
+    epochs=50 #was 100
     learning_rate=2.0
     file=1
-    filenames = "cifar100_mem=100"
+    filenames = "CIFAR10_class=1_mem=50_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
