e96dd73ed2af736b10c61638bd34143fe93840c3
kenjacob <kenjacob0606@gmail.com>
2026-04-20 03:27:14 +0100

    .

--- main_2.py
+++ main_2.py
@@ -8,7 +8,7 @@ import time
 
 #   cifar10_epo_15_class=1_mem=2K_def
 
-for train_no in range(3,4):
+for train_no in range(1,4):
     dataset='CIFAR10' #try other dataset
     numclass=1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
@@ -20,10 +20,10 @@ for train_no in range(3,4):
     batch_size=128  
     task_size=1 #num of classes learned each task
     memory_size= 2000
-    epochs=35 #was 100
+    epochs=45 #was 100
     learning_rate=2.0
     file=1
-    filenames = "cifar10_epo_35_class=1_mem=2K_def"
+    filenames = "cifar10_epo_45_class=1_mem=2K_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
