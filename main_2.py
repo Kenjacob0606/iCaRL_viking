a8cab7954e42ed78edac125e70254cd043333049
kenjacob <kenjacob0606@gmail.com>
2026-04-20 21:23:17 +0100

    .

--- main_2.py
+++ main_2.py
@@ -1,4 +1,4 @@
-from iCaRL_2 import iCaRLmodel
+from iCaRL_cifar10_epo50 import iCaRLmodel
 from ResNet import resnet18_cbam
 from ResNet import resnet18_MNIST_cbam
 from ResNet import resnet34_cbam
@@ -19,11 +19,11 @@ for train_no in range(1,4):
     img_size=32
     batch_size=128  
     task_size=1 #num of classes learned each task
-    memory_size= 2000
+    memory_size= 1000
     epochs=50 #was 100
     learning_rate=2.0
     file=1
-    filenames = "cifar10_epo_50_class=1_mem=2K_def"
+    filenames = "cifar10_epo_50_class=1_mem=1K_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
