3b9e4bb2b1ad9331b8098d1fd50ab65c7cb77e1c
kenjacob <kenjacob0606@gmail.com>
2026-04-24 10:21:24 +0100

    .

--- main_MNIST_15epo_2.py
+++ main_MNIST_15epo_2.py
@@ -10,7 +10,7 @@ import time
 
 for train_no in range (1,4):
     dataset='MNIST' #try other dataset
-    numclass=2      #num of classes learned initially, will be updated in incremental learning
+    numclass=1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
         numclasses = 100
     else:
@@ -18,12 +18,12 @@ for train_no in range (1,4):
     feature_extractor=resnet18_cbam(num_classes=numclasses) #try other resnets
     img_size=32
     batch_size=128  
-    task_size=2         #num of classes learned each task
+    task_size=1         #num of classes learned each task
     memory_size= 1000
     epochs=15 #was 100
-    learning_rate=2.0
+    learning_rate=1.5
     file=1
-    filenames = "MNIST_class=2_mem=1K_def"
+    filenames = "MNIST_class=1_lr=1.5_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
