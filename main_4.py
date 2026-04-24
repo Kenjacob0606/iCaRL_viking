38da37f95f39a9cdc06e932268d1ad44445e7294
kenjacob <kenjacob0606@gmail.com>
2026-04-25 00:54:59 +0100

    .

--- main_4.py
+++ main_4.py
@@ -10,7 +10,7 @@ import time
 
 #TRAIN 1
 
-for train_no in range(1,4):
+for train_no in range(3,4):
     dataset='CIFAR10' #try other dataset
     numclass=1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
