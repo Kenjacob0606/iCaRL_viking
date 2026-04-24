e499ecd7d5e73c999790b37c50092fe8812ea629
kenjacob <kenjacob0606@gmail.com>
2026-04-24 13:44:29 +0100

    .

--- main_4.py
+++ main_4.py
@@ -21,11 +21,11 @@ for train_no in range(1,4):
     img_size=32
     batch_size=128  
     task_size=  1           #num of classes learned each task
-    memory_size= 200
+    memory_size= 2000
     epochs=50 #was 100
-    learning_rate=2.0
+    learning_rate=1.5
     file=1
-    filenames = "CIFAR10_class=1_mem=200_def"
+    filenames = "CIFAR10_lr=1.5_def"
 
 
     model=iCaRLmodel(numclass,feature_extractor,batch_size,task_size,memory_size,epochs,learning_rate,dataset,file,train_no,filenames) #try other dataset
