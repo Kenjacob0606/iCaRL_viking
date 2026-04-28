a87ef31dd0f3b5713fb6c7fdc3df17341573ec7f
kenjacob <kenjacob0606@gmail.com>
2026-04-28 06:45:59 +0100

    .

--- main_2.py
+++ main_2.py
@@ -8,7 +8,7 @@ import time
 
 #   cifar10_epo_15_class=1_mem=2K_def
 
-for train_no in range(1,4):
+for train_no in range(2,4):
     dataset='CIFAR10' #try other dataset
     numclass= 1      #num of classes learned initially, will be updated in incremental learning
     if dataset == 'CIFAR100':
