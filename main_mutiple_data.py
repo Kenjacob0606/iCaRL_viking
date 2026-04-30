753604fbac7b94284363c047c382bcec24aeeb9b
kenjacob <kenjacob0606@gmail.com>
2026-04-30 17:40:38 +0100

    .

--- main_mutiple_data.py
+++ main_mutiple_data.py
@@ -1,4 +1,4 @@
-from iCaRL_multipledata import iCaRLmodel
+from iCaRL_mul_LATEST import iCaRLmodel
 from ResNet import resnet18_cbam
 from ResNet import resnet18_MNIST_cbam
 from ResNet import resnet34_cbam
